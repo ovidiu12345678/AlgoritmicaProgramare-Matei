53d04a7ddcba6c9fc9e056492b6ec2b42faf3242
Ovidiu Anton Anton OVIDIUs <ovidiugabriel123456789@outlook.com>
2022-06-12 23:09:08 +0300

    Add project files.

---
+++ AlgoritmicaProgramare Matei/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoritmicaProgramare_Matei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
